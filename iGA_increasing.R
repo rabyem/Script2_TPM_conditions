46ee492bf3d10150e8a7c0aa8ed27aaf1ce5b893
rabyem <59205097+rabyem@users.noreply.github.com>
2020-04-15 20:13:39 +0100

    Add files via upload

---
+++ iGA_increasing.R
@@ -0,0 +1,19 @@
+setwd("C:/Users/Owner/Script2_iGA")
+source("iGAaccurate.R")
+source("db_iGAaccurate.R")
+library(Rmpfr)
+
+metric <- read.csv("metric.csv", row.names = 1, header = F)
+group_membership <- read.csv("group_membership.csv", row.names = 1)
+group_membership <- as.matrix(group_membership)
+groups <- colnames(group_membership)
+decreasing <- F
+var.names <- rownames(metric)
+
+out<- iGA_acc(metric = metric[,1],
+              group.membership = group_membership,
+              var.names = var.names,
+              groups = groups,
+              decreasing = decreasing)
+
+write.csv(out$summary, file="outputiGA_tmp.csv", row.names = F)
